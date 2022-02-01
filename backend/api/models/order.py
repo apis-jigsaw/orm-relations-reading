0a839154251faecf2f72e3eab197192f4b0f114b
JeffKatzy <jeffkatzygit config --global user.name JeffKatzy>
2022-02-01 17:36:31 -0500

    update code

---
+++ backend/api/models/order.py
@@ -0,0 +1,4 @@
+class Order:
+    __table__ = 'orders'
+    columns = ['id', 'customer_id', 'drink_id', 'bartender_id']
+    
---
+++ backend/console.py
@@ -0,0 +1,6 @@
+from api.lib.db import *
+from api.lib.orm import *
+from api.models.bartender import Bartender
+from api.models.drink import Drink
+from api.models.order import Order
+from api.models.customer import Customer
